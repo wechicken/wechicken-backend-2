812ce4ed0a0483bc08b24e057f16039dd6ce34fa
ohogeun <dhgh95>
2021-10-08 21:47:15 +0900

    Feat: batch module swagger 작업

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,10 @@ async function bootstrap() {
     .setTitle('WECHICKEN')
     .setDescription('The Wechicken API description')
     .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+      'authorization',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
