d2be1746e9f833bf31862f99e20b52116476f2ab
Jun4928 <jun.choi.4928@gmail.com>
2021-10-26 23:06:43 +0900

    Add: eventContext middleware

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,13 +1,19 @@
 import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
 import { Handler, Context } from 'aws-lambda';
-import { defaultApp, setupSwagger } from './main';
+import { setupSwagger } from './main';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 let cachedServer: Handler;
 
 async function bootstrap(): Promise<Handler> {
   if (!cachedServer) {
     try {
-      const app = await defaultApp();
+      const app = await NestFactory.create(AppModule);
+      app.use(eventContext());
+      app.useGlobalPipes(new ValidationPipe());
       setupSwagger(app);
       await app.init();
 
@@ -29,6 +35,11 @@ export const handler: Handler = async (event: any, context: Context) => {
   event.path = event.path.includes('swagger-ui')
     ? `/api${event.path}`
     : event.path;
+
+  console.log(event);
+  console.log(event.headers);
+  console.log(event.path);
+  console.log(process.env.STAGE);
 
   cachedServer = await bootstrap();
   return proxy(cachedServer, event, context, 'PROMISE').promise;
