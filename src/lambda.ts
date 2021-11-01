544531e5827101555d1dd0d7b1112a1201a3746e
Jun4928 <jun.choi.4928@gmail.com>
2021-11-01 20:11:15 +0900

    Modify: to the point where the APIs work

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,44 +1,23 @@
-import { createServer, proxy } from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
-import { Handler, Context } from 'aws-lambda';
-import { setupSwagger } from './main';
+import { Handler, Context, Callback } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
 let cachedServer: Handler;
 
 async function bootstrap(): Promise<Handler> {
-  if (!cachedServer) {
-    try {
-      const expressApp = express();
-      const adapter = new ExpressAdapter(expressApp);
-      const app = await NestFactory.create(AppModule, adapter);
-      app.use(eventContext());
-      app.use(express.json());
-      app.useGlobalPipes(new ValidationPipe());
-      // setupSwagger(app);
-      await app.init();
-      cachedServer = createServer(expressApp, undefined, []);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  }
+  const app = await NestFactory.create(AppModule);
+  await app.init();
 
-  return cachedServer;
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 
-export const handler: Handler = async (event: any, context: Context) => {
-  // if (event.path === '/api') {
-  //   event.path = '/api/';
-  // }
-
-  // event.path = event.path.includes('swagger-ui')
-  //   ? `/api${event.path}`
-  //   : event.path;
-
-  cachedServer = await bootstrap();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return cachedServer(event, context, callback);
 };
